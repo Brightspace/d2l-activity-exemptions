2a0bf88962f6d0c6bfdb240c22678ab600c7a66c
Joseph Wong <Joseph.Wong@D2L.com>
2019-08-26 14:09:36 -0400

    fix tests

--- test/d2l-localize-behavior/test-elements.js
+++ test/d2l-localize-behavior/test-elements.js
@@ -1,9 +1,9 @@
-import '../../src/d2l-localize-behavior.js';
+import '../../localize-behavior.js';
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 import { dom } from '@polymer/polymer/lib/legacy/polymer.dom.js';
 Polymer({
 	is: 'test-localize',
-	behaviors: [ D2L.PolymerBehaviors.ActivityExemptions.LocalizeBehavior ],
+	behaviors: [ D2L.PolymerBehaviors.D2LActivityExemptions.LocalizeBehavior ],
 	ready: function() {
 		dom(this.root).appendChild(
 			document.createTextNode(
